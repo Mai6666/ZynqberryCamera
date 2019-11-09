5a181831808ef7dbe0fcd98aa2d27897df153a2a
Mai6666 <primoliebe@yahoo.co.jp>
2019-11-09 14:53:00 +0900

    intに変更

--- c/rgb2y.cpp
+++ c/rgb2y.cpp
@@ -1,22 +1,19 @@
 #include "manga_fil.h"
-#include <stdio.h>
-#include <stdlib.h>
-typedef unsigned char byte;
-typedef unsigned char ubyte;
 
 int rgb2y(int pix){
-  ubyte R,G,B;
+  int R,G,B;
   int value;
 
-   R =(ubyte)((pix >> 16) & 0xff);
-   G =(ubyte)((pix >> 8) & 0xff);
-   B =(ubyte)(pix  & 0xff);
-    
+   R =((pix >> 16) & 0xff);
+   G =((pix >> 8) & 0xff);
+   B =(pix  & 0xff);
+
   if(R==G && G==B){
     return R;
+    std::cout << "if" << std::endl;
   }
   else{
-    byte value = (byte)(0.299 * R + 0.587 * G + 0.144 * B);
+    value = (0.299 * R + 0.587 * G + 0.144 * B);
     return value;
   }
 }
